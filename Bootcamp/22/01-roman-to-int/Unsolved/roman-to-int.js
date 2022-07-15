2c60cd70ea5cd72ea1cea1a54a13767518974c44
Neo <fullstackneo@gmail.com>
2022-07-15 03:50:13 -0600

    complete all

--- Bootcamp/22/01-roman-to-int/Unsolved/roman-to-int.js
+++ Bootcamp/22/01-roman-to-int/Unsolved/roman-to-int.js
@@ -1,3 +1,33 @@
 // Write a function that takes in a Roman Numeral string and returns its integer form
 
-var romanToInt = function(str) {};
+var romanToInt = function (str) {
+  // set map for 1-9 ,total=0
+  let total = 0;
+  const numMap = {
+    I: 1,
+    II: 2,
+    III: 3,
+    IV: 4,
+    V: 5,
+    VI: 6,
+    VII: 7,
+    VIII: 8,
+    IX: 9,
+    X: 10,
+    C: 100,
+    M: 1000,
+  };
+
+  // loop the arr, M, C, X, add to total
+  str.split('').some((el, index, arr) => {
+    if (['I', 'V'].includes(el)) {
+      total += numMap[arr.slice(index).join('')];
+      return true;
+    }
+    total += numMap[el];
+  });
+  // add the remaing to total
+  return total;
+};
+
+console.log(romanToInt('V'));
--- Bootcamp/22/02-rotation-point/Unsolved/rotation-point.js
+++ Bootcamp/22/02-rotation-point/Unsolved/rotation-point.js
@@ -1,3 +1,16 @@
 // Write a function that takes in an array of sorted strings and returns the index of its rotation point if it has one, else return -1
 
-var rotationPoint = function(words) {};
+var rotationPoint = function (words) {
+  // if last > first, return -1
+  if (words[words.length - 1] > words[0]) {
+    return -1;
+    // if last <= first,
+  } else {
+    return words.findIndex((el, index) => {
+      if (index === 0) return;
+      return words[index - 1] > el;
+    });
+  }
+};
+
+console.log(rotationPoint(['cool', 'cat']));
