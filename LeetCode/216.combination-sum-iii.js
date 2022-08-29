67bd3d81196d2b9c9d223b29c190ecabfd9c2a2a
Neo <fullstackneo@gmail.com>
2022-08-29 02:02:03 -0600

    216 DFS

--- LeetCode/216.combination-sum-iii.js
+++ LeetCode/216.combination-sum-iii.js
@@ -10,8 +10,18 @@
  * @param {number} n
  * @return {number[][]}
  */
-var combinationSum3 = function(k, n) {
-    
+var combinationSum3 = function (k, n) {
+  let result = [];
+
+  const process = (arr, cur, sum) => {
+    if (arr.length > k || sum > n) return;
+    if (arr.length === k && sum === n) return result.push(arr);
+    for (let i = cur + 1; i <= 9; i++) {
+      process([...arr, i], i, sum + i);
+    }
+  };
+
+  process([], 0, 0);
+  return result;
 };
 // @lc code=end
-
